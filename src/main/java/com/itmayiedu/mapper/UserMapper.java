1f77e28a2b4bfde30a4f90254a043d2d4c6ab2ae
liuwenqiang <liuwq@tydic.com>
2018-11-06 16:55:24 +0800

    初始化

---
+++ src/main/java/com/itmayiedu/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.itmayiedu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itmayiedu.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
